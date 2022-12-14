1b64c4c086d0e7c590a3de1124c1c75669a633d5
Johan Andersson <johan.andersson@rubrik.com>
2022-12-14 17:23:01 +0100

    Add example showing how to add multiple AWS accounts
    The accounts are kept in a CSV file.

---
+++ aws_accounts/main.tf
@@ -0,0 +1,57 @@
+# Point Terraform to the RSC provider.
+terraform {
+  required_providers {
+    polaris = {
+      source  = "rubrikinc/polaris"
+      version = "~>0.3.0"
+    }
+  }
+}
+
+variable "polaris_credentials" {
+  type        = string
+  description = "Path to the RSC service account file."
+}
+
+# The accounts.csv file should contain all AWS accounts to add to RSC, using the
+# following format:
+#
+# profile,regions
+# "<profile-1>","<region-1>,<region-2>...<region-N>"
+# "<profile-2>","<region-1>,<region-2>...<region-N>"
+#
+# E.g:
+#
+# profile,regions
+# "admin","us-east-2,us-west-2"
+# "devops","us-west-2,eu-north-1"
+#
+# The header, the first line of the CSV file, must also be included in the CSV
+# file.
+#
+# After the accounts have been added they can be managed through the CSV file.
+# Removing an account from the CSV file followed by running terraform apply will
+# remove the account from RSC. Changing the regions for an account followed by
+# running terraform apply will update the regions in RSC.
+locals {
+  content  = file("accounts.csv")
+  accounts = csvdecode(local.content)
+}
+
+# Point the provider to the RSC service account to use.
+provider "polaris" {
+  credentials = var.polaris_credentials
+}
+
+# Add the AWS accounts from the accounts.csv file to RSC. Access keys and secret
+# keys for profiles are read from ~/.aws/credentials. The default region for the
+# profiles are read from ~/.aws/config. RSC will authenticate to AWS using an
+# IAM role setup in a CloudFormation stack.
+resource "polaris_aws_account" "account" {
+  for_each = { for account in local.accounts : account.profile => account }
+  profile  = each.key
+
+  cloud_native_protection {
+    regions = split(",", each.value.regions)
+  }
+}
