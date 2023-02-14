9aa3297d46b00b24f66c0b273f618154611d20b2
Johan Andersson <johan.andersson@rubrik.com>
2023-02-14 14:39:51 +0100

    Add AWS MFA example

---
+++ aws_with_mfa/main.tf
@@ -0,0 +1,38 @@
+# Point Terraform to the RSC provider.
+terraform {
+  required_providers {
+    polaris = {
+      source  = "terraform.rubrik.com/rubrikinc/polaris"
+      version = "~>0.5.0"
+    }
+  }
+}
+
+variable "polaris_credentials" {
+  type        = string
+  description = "Path to the RSC service account file."
+}
+
+variable "profile" {
+  type        = string
+  default     = "default"
+  description = "Name of temporary profile or default for environment variables."
+}
+
+# Point the provider to the RSC service account to use.
+provider "polaris" {
+  credentials = var.polaris_credentials
+}
+
+# Add the AWS account to RSC. Access key and secret key are read from
+# ~/.aws/credentials. The default region is read from ~/.aws/config. RSC will
+# authenticate to AWS using an IAM role setup in a CloudFormation stack.
+resource "polaris_aws_account" "default" {
+  profile = var.profile
+
+  cloud_native_protection {
+    regions = [
+      "us-east-2",
+    ]
+  }
+}
