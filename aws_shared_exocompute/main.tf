62f107b96e0eb5d5df53bf5e67e7870f8ef1d689
Johan Andersson <johan.andersson@rubrik.com>
2023-11-16 10:09:58 +0100

    Add support for AWS shared exocompute

---
+++ aws_shared_exocompute/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    polaris = {
+      source  = "terraform.rubrik.com/rubrikinc/polaris"
+      version = "=0.8.0-beta.6"
+    }
+  }
+}
+
+variable "profile" {
+  type        = string
+  description = "AWS profile."
+}
+
+variable "host_account_id" {
+  type        = string
+  description = "Shared exocompute host RSC account id."
+}
+
+provider "polaris" {}
+
+resource "polaris_aws_account" "default" {
+  profile = var.profile
+
+  cloud_native_protection {
+    regions = [
+      "us-east-2",
+    ]
+  }
+
+  exocompute {
+    regions = [
+      "us-east-2",
+    ]
+  }
+}
+
+resource "polaris_aws_exocompute" "default" {
+  account_id      = polaris_aws_account.default.id
+  host_account_id = var.host_account_id
+}
