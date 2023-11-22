108ef55554beaa4822a5ebfcb277b5611f0390e5
Johan Andersson <johan.andersson@rubrik.com>
2023-11-22 14:24:56 +0100

    Add example for AWS private container registry (#19)

---
+++ aws_private_container_registry/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    polaris = {
+      source  = "rubrikinc/polaris"
+      version = "=v0.8.0-beta.6"
+    }
+  }
+}
+
+variable "profile" {
+  type        = string
+  description = "AWS profile."
+}
+
+# The RSC service account is read from the environment variable
+# RUBRIK_POLARIS_SERVICEACCOUNT_CREDENTIALS.
+provider "polaris" {}
+
+# Add the AWS account to RSC.
+resource "polaris_aws_account" "default" {
+  profile = var.profile
+
+  cloud_native_protection {
+    regions = [
+      "us-east-2",
+    ]
+  }
+}
+
+# Inform RSC about the private container registry.
+resource "polaris_aws_private_container_registry" "default" {
+  account_id = polaris_aws_account.default.id
+  url        = "123456789012.dkr.ecr.us-east-2.amazonaws.com"
+}
