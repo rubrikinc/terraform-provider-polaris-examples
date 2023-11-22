42c89ae5a1bc736c968af26b5807b3a2c5cca50d
Johan Andersson <johan.andersson@rubrik.com>
2023-11-22 13:57:31 +0100

    Add example for AWS cloud archival location

---
+++ aws_archival_location/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    polaris = {
+      source  = "rubrikinc/polaris"
+      version = "=0.8.0-beta.7"
+    }
+  }
+}
+
+variable "cloud_account_id" {
+  type        = string
+  description = "RSC cloud account ID."
+}
+
+variable "profile" {
+  type        = string
+  description = "AWS profile."
+}
+
+# The RSC service account is read from the environment variable
+# RUBRIK_POLARIS_SERVICEACCOUNT_CREDENTIALS.
+provider "polaris" {}
+
+resource "polaris_aws_archival_location" "default" {
+  account_id    = var.cloud_account_id
+  name          = "my-archival-location"
+  bucket_prefix = "bucket-f48wad7flz"
+  storage_class = "STANDARD"
+}
