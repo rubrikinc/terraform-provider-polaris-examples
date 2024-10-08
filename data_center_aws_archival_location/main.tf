5b6a0c74b117c18abee1debbc9dbd0a050e43fbf
Johan Andersson <johan.andersson@rubrik.com>
2024-10-08 08:39:46 +0200

    Remove time_sleep from Amazon S3 DC archival location example

--- data_center_aws_archival_location/main.tf
+++ data_center_aws_archival_location/main.tf
@@ -5,15 +5,11 @@ terraform {
   required_providers {
     polaris = {
       source  = "rubrikinc/polaris"
-      version = "=0.10.0-beta.4"
+      version = "=0.10.0-beta.5"
     }
     random = {
       source  = "hashicorp/random"
       version = "~>3.6.0"
-    }
-    time = {
-      source = "hashicorp/time"
-      version = "~>0.12.0"
     }
   }
 }
@@ -90,16 +86,6 @@ resource "random_string" "bucket_suffix" {
   upper   = false
 }
 
-# We add a 60 second sleep when destroying the resources to allow for the
-# archival location to be destroyed before the the data center account.
-resource "time_sleep" "wait_60_seconds" {
-  destroy_duration = "60s"
-
-  depends_on = [
-    polaris_data_center_aws_account.account,
-  ]
-}
-
 resource "polaris_data_center_archival_location_amazon_s3" "archival_location" {
   name             = var.archival_location_name
   region           = var.region
@@ -107,8 +93,4 @@ resource "polaris_data_center_archival_location_amazon_s3" "archival_location" {
   cloud_account_id = polaris_data_center_aws_account.account.id
   cluster_id       = var.cluster_id
   rsa_key          = var.rsa_key
-
-  depends_on = [
-    time_sleep.wait_60_seconds,
-  ]
 }
