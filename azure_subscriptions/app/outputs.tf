a89c3c42f15f63610dac05deb155388f67b3f6ae
Johan Andersson <johan.andersson@rubrik.com>
2024-11-25 19:56:05 +0100

    Remove non-working example

--- azure_subscriptions/app/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "principal_id" {
-  value = azuread_service_principal.service_principal.object_id
-}
-
-output "tenant_domain" {
-  value = polaris_azure_service_principal.service_principal.tenant_domain
-}
