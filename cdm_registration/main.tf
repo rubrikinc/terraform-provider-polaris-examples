ac7fe4ff39eedbc8ea5127036458d1216a01948e
Johan Andersson <johan.andersson@rubrik.com>
2025-02-04 22:50:47 +0100

    Add CDM cluster registration example

---
+++ cdm_registration/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    polaris = {
+      source  = "rubrikinc/polaris"
+      version = "=1.1.0-beta.1"
+    }
+  }
+}
+
+variable "admin_password" {
+  description = "Password for the cluster admin account."
+  type        = string
+  sensitive   = true
+}
+
+variable "cluster_name" {
+  description = "Cluster name."
+  type        = string
+}
+
+variable "cluster_node_ip_address" {
+  description = "Cluster node IP address."
+  type        = string
+}
+
+provider "polaris" {}
+
+resource "polaris_cdm_registration" "cdm_registration" {
+  admin_password          = var.admin_password
+  cluster_name            = var.cluster_name
+  cluster_node_ip_address = var.cluster_node_ip_address
+}
