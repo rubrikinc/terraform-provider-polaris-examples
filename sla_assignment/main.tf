ca5e138e20410e18e86485bb08df2b88e55e7bda
Johan Andersson <johan.andersson@rubrik.com>
2025-04-10 14:22:04 +0200

    Add RSC SLA domain assignment example (#53)

---
+++ sla_assignment/main.tf
@@ -0,0 +1,40 @@
+# Example showing how to assign a role to an RSC user.
+
+terraform {
+  required_providers {
+    polaris = {
+      source  = "rubrikinc/polaris"
+      version = "=1.1.0-beta.6"
+    }
+  }
+}
+
+data "polaris_sla_domain" "bronze" {
+  name = "bronze"
+}
+
+# Create a tag rule for AWS EC2 instances.
+resource "polaris_tag_rule" "aws_bronze" {
+  name        = "aws-bronze"
+  object_type = "AWS_EC2_INSTANCE"
+  tag_key     = "backup"
+  tag_value   = "true"
+}
+
+# Create a tag rule for Azure VM instances.
+resource "polaris_tag_rule" "azure_bronze" {
+  name        = "azure-bronze"
+  object_type = "AZURE_VIRTUAL_MACHINE"
+  tag_key     = "backup"
+  tag_value   = "true"
+}
+
+# Assign the tag rules to the bronze SLA domain.
+resource "polaris_sla_domain_assignment" "bronze" {
+  sla_domain_id = data.polaris_sla_domain.bronze.id
+
+  object_ids = [
+    polaris_tag_rule.aws_bronze.id,
+    polaris_tag_rule.azure_bronze.id,
+  ]
+}
