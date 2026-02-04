5d7064f0c9c66fb38a2b074b4aa266601a8e36bb
Johan Andersson <johan.andersson@rubrik.com>
2026-02-04 20:29:24 +0100

    Wait for account to onboard before creating shared Exo config

--- modules/aws_iam_account/exocompute.tf
+++ modules/aws_iam_account/exocompute.tf
@@ -2,4 +2,8 @@ resource "polaris_aws_exocompute" "exocompute" {
   count           = var.exocompute_host_id == null ? 0 : 1
   account_id      = polaris_aws_cnp_account.account.id
   host_account_id = var.exocompute_host_id
+
+  depends_on = [
+    time_sleep.wait_for_rsc,
+  ]
 }
